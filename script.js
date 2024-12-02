47eb169812e9a623e22f80e928e72fee21dab6ad
Zfolkes <zlfa2021@mymail.pomona.edu>
2024-12-02 11:45:33 -0800

    took away some sliders

--- script.js
+++ script.js
@@ -263,13 +263,6 @@ document.getElementById('color1saturation').addEventListener('input', (event) =>
 }
 });
 
-//TODO: Figure out how tint and shade should relate
-document.getElementById('color1shade').addEventListener('input', (event) => {
-  color1HSL.l = Math.min(50, Math.max(0, parseInt(event.target.value)));
-  updateCircleColor('circle1', color1HSL);
-  updateMixedColorDisplay();
-});
-
 document.getElementById('color2tint').addEventListener('input', (event) => {
   if (isActiveCircle('circle2')&& circle2Audio){
   color2HSL.l = Math.min(100, Math.max(50, parseInt(event.target.value)));
@@ -281,11 +274,6 @@ document.getElementById('color2tint').addEventListener('input', (event) => {
       console.log(`Speed (Tint) for Circle 2 audio set to: ${speed}`);
     
   }
-});
-
-document.getElementById('color2shade').addEventListener('input', (event) => {
-  color2HSL.l = Math.min(50, Math.max(0, parseInt(event.target.value)));
-  updateCircleColor('circle2', color2HSL);
 });
 
 document.getElementById('color2saturation').addEventListener('input', (event) => {
